4af354dd50314f426f6a856fe1987899b871c4c8
ponomoly <ponomoly.dev@gmail.com>
2023-12-01 17:34:45 +0900

    feat: init styled-components

---
+++ src/components/TestStyledComponent.tsx
@@ -0,0 +1,11 @@
+"use client";
+import styled from "styled-components";
+
+const TestStyledComponent = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 200px;
+  color: yellow;
+`;
+
+export default TestStyledComponent;
